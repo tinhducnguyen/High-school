cf666c1d3df5fd7be5ea24af68d8625a942ca2bc
tinh <you@example.com>
2021-05-27 12:39:40 +0700

    first commit

---
+++ admin/dangnhap.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Đăng Nhập</title>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="topNav">
+		<div class="container">
+			<div class="alignR">
+                 <a href="http://localhost/diemthi/index.html" style="font-size:14px">Trang Chủ</a>
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="banner">
+<center><img src="../assets/img/Ban.png"></center>
+<body bgcolor="#CAFFFF">
+<?php
+echo '<br/>';
+?>
+<?php
+?>
+<style type="text/css">
+#menu ul{
+	margin-left:145px;
+	text-align: center;
+
+}
+.menu{
+
+	}
+#menu ul li{
+display:inline;
+
+}
+#menu ul a{
+text-decoration:none;
+	width: 490px;
+float:left;
+background:#336699;
+color:#FFFFFF;
+text-align:center;
+line-height:27px;
+font-weight:bold;
+border-left:1px solid #FFFFFF;
+}
+
+#menu ul a:hover{
+background:#000000;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="style.css">
+<div id="menu" >
+		<p style="font-family:Tahoma;font-weight: bold;text-align: center;font-size: large">CHÀO MỪNG BẠN ĐẾN TRANG ĐĂNG NHẬP</p>
+    	<ul>
+        	<li><a href="login.php">Đăng Nhập Quản Trị Hệ Thống</a></li>
+			<li><a href="logingv.php">Đăng Nhập Giáo Viên</a></li>
+		</ul>
+</div>
+<table  border=0 cellpadding=5 cellspacing=0 align="center" width="983">
+	<TR>
+		<TD>	<tr>
+		<td  colspan="2" bgcolor="#336699" align="center" style="color:white" >
+		 Copyright &copy; 20021 Trường THPT PHÙ CỪ <br>		
+		</td>
+	</tr>
+</td>
+	</TR>
+</table>
+</body>
+
+</body>
+</html>
